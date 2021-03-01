9b8ec9aed4dd874f297e0e8812bc618c0dfbaf26
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-03-01 14:44:37 +0530

    router delegate implemented

--- lib/router/router_delegate.dart
+++ lib/router/router_delegate.dart
@@ -1,21 +1,132 @@
-import 'package:agri_guide/router/ui_pages.dart';
+import 'package:agri_guide/app/accounts/presentation/login/login_view.dart';
+import 'package:agri_guide/app/accounts/presentation/register/register_view.dart';
+import 'package:agri_guide/app/home/presentation/home_view.dart';
+import 'package:agri_guide/app/splash/presentation/splash_view.dart';
 import 'package:flutter/material.dart';
+
+import 'ui_pages.dart';
 
 class AgriGuideRouterDelegate extends RouterDelegate<PageConfiguration>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<PageConfiguration> {
+  final List<Page> _pages = [];
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  PageConfiguration get currentConfiguration =>
+      _pages.last.arguments as PageConfiguration;
+
+  @override
+  Future<bool> popRoute() {
+    if (_pages.length > 1) {
+      _removePage(_pages.last);
+      return Future.value(true);
+    }
+    return Future.value(false);
+  }
+
+  MaterialPage _createPage(Widget child, PageConfiguration pageConfig) {
+    return MaterialPage(
+      child: child,
+      key: Key(pageConfig.key),
+      name: pageConfig.path,
+      arguments: pageConfig,
+    );
+  }
+
+  void _addPageData(Widget child, PageConfiguration pageConfig) {
+    _pages.add(_createPage(
+      child,
+      pageConfig,
+    ));
   }
 
   @override
-  // TODO: implement navigatorKey
-  GlobalKey<NavigatorState> get navigatorKey => throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _onPopPage,
+      pages: List.of(_pages),
+    );
+  }
+
+  @override
+  GlobalKey<NavigatorState> get navigatorKey => GlobalKey<NavigatorState>();
+
+  void replace(PageConfiguration newRoute) {
+    if (_pages.isNotEmpty) {
+      _pages.removeLast();
+    }
+    addPage(newRoute);
+  }
+
+  void setPath(List<MaterialPage> path) {
+    _pages.clear();
+    _pages.addAll(path);
+    notifyListeners();
+  }
+
+  void replaceAll(PageConfiguration newRoute) {
+    setNewRoutePath(newRoute);
+  }
+
+  void push(PageConfiguration newRoute) {
+    addPage(newRoute);
+  }
+
+  void pushWidget(Widget child, PageConfiguration newRoute) {
+    _addPageData(child, newRoute);
+    notifyListeners();
+  }
 
   @override
   Future<void> setNewRoutePath(PageConfiguration configuration) {
-    // TODO: implement setNewRoutePath
-    throw UnimplementedError();
+    _pages.clear();
+    addPage(configuration);
+    return Future.value(null);
+  }
+
+  void addPage(PageConfiguration pageConfig) {
+    final shouldAddPage = _pages.isEmpty ||
+        (_pages.last.arguments as PageConfiguration).uiPage !=
+            pageConfig.uiPage;
+
+    if (shouldAddPage) {
+      switch (pageConfig.uiPage) {
+        case Pages.Splash:
+          _addPageData(SplashPage(), splashPageConfig);
+          break;
+        case Pages.Login:
+          _addPageData(LoginPage(), loginPageConfig);
+          break;
+        case Pages.Register:
+          _addPageData(RegisterPage(), registerPageConfig);
+          break;
+        case Pages.Home:
+          _addPageData(HomePage(), homePageConfig);
+          break;
+
+        default:
+          break;
+      }
+
+      notifyListeners();
+    }
+  }
+
+  bool _onPopPage(Route<dynamic> route, result) {
+    final didPop = route.didPop(result);
+    if (!didPop) {
+      return false;
+    }
+
+    _pages.remove(route.settings);
+    notifyListeners();
+    return true;
+  }
+
+  void _removePage(MaterialPage page) {
+    if (page != null) {
+      _pages.remove(page);
+    }
+    notifyListeners();
   }
 }
