ca84c82f45722f3bc9935f2142506cb557988667
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-03-04 15:51:24 +0530

    created usecases

---
+++ lib/app/dashboard/domain/usecase/fetch_live_weather_usecase.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+import '../entity/live_weather_entity.dart';
+import '../repository/dashboard_services_repository.dart';
+
+class FetchLiveWeatherUsecase extends CompletableUseCase<void> {
+  DashboardServicesRepository _repository;
+
+  FetchLiveWeatherUsecase(this._repository);
+
+  @override
+  Future<Stream<LiveWeatherEntity>> buildUseCaseStream(void params) async {
+    StreamController<LiveWeatherEntity> streamController = StreamController();
+
+    try {
+      LiveWeatherEntity liveWeatherEntity =
+          await _repository.fetchLiveWeather();
+      print('Live weather fetched');
+      streamController.add(liveWeatherEntity);
+      streamController.close();
+    } catch (error) {
+      print('Live weather not fetched');
+      streamController.addError(error);
+    }
+
+    return streamController.stream;
+  }
+}
---
+++ lib/app/dashboard/domain/usecase/fetch_location_details_usecase.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+import '../entity/location_entity.dart';
+import '../repository/dashboard_services_repository.dart';
+
+class FetchLocationDetailsUsecase extends CompletableUseCase<void> {
+  DashboardServicesRepository _repository;
+
+  FetchLocationDetailsUsecase(this._repository);
+
+  @override
+  Future<Stream<LocationEntity>> buildUseCaseStream(void params) async {
+    StreamController<LocationEntity> streamController = StreamController();
+
+    try {
+      LocationEntity locationEntity =
+          await _repository.fetchLatitudeAndLongitude();
+      print('Location details fetched');
+      streamController.add(locationEntity);
+      streamController.close();
+    } catch (error) {
+      print('Location details not fetched');
+      streamController.addError(error);
+    }
+
+    return streamController.stream;
+  }
+}
