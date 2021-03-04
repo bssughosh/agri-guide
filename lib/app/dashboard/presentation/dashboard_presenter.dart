327e6d1b30bdc461157b719be9e87b3683f74713
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-03-04 15:57:51 +0530

    added presenter

---
+++ lib/app/dashboard/presentation/dashboard_presenter.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+import '../../../core/observer.dart';
+import '../../accounts/domain/usecase/check_login_status_usecase.dart';
+import '../domain/usecase/fetch_live_weather_usecase.dart';
+import '../domain/usecase/fetch_location_details_usecase.dart';
+
+class DashboardPagePresenter extends Presenter {
+  FetchLiveWeatherUsecase _fetchLiveWeatherUsecase;
+  FetchLocationDetailsUsecase _fetchLocationDetailsUsecase;
+  CheckLoginStatusUsecase _checkLoginStatusUsecase;
+
+  DashboardPagePresenter(
+    this._checkLoginStatusUsecase,
+    this._fetchLiveWeatherUsecase,
+    this._fetchLocationDetailsUsecase,
+  );
+
+  @override
+  dispose() {
+    _fetchLocationDetailsUsecase.dispose();
+    _fetchLiveWeatherUsecase.dispose();
+    _checkLoginStatusUsecase.dispose();
+  }
+
+  void checkLoginStatus(UseCaseObserver observer) {
+    _checkLoginStatusUsecase.execute(observer);
+  }
+
+  void fetchLocationDetails(UseCaseObserver observer) {
+    _fetchLocationDetailsUsecase.execute(observer);
+  }
+
+  void fetchLiveWeather(UseCaseObserver observer) {
+    _fetchLiveWeatherUsecase.execute(observer);
+  }
+}
