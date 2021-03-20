1a93673492892cf6977f670e590a325b2b14d7a3
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-03-20 16:32:46 +0530

    display done

--- lib/app/prediction/presentation/mobile/display_initialized_view.dart
+++ lib/app/prediction/presentation/mobile/display_initialized_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/app_theme.dart';
+import '../../../../core/enums.dart';
 import '../prediction_controller.dart';
+import '../widgets/custom_table.dart';
 
 Widget buildPredictionDisplayInitializedViewMobile({
   @required PredictionPageController controller,
@@ -12,7 +15,72 @@ Widget buildPredictionDisplayInitializedViewMobile({
       width: MediaQuery.of(context).size.width * 0.9,
       child: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            if (controller.isPredicting) CircularProgressIndicator(),
+            if (!controller.isPredicting && controller.temperature.length > 0)
+              CustomTable(
+                controller: controller,
+                tableType: TableType.TEMPERATURE,
+                isWeb: true,
+              ),
+            SizedBox(height: 15),
+            if (!controller.isPredicting && controller.humidity.length > 0)
+              CustomTable(
+                controller: controller,
+                tableType: TableType.HUMIDITY,
+                isWeb: true,
+              ),
+            SizedBox(height: 15),
+            if (!controller.isPredicting && controller.rainfall.length > 0)
+              CustomTable(
+                controller: controller,
+                tableType: TableType.RAINFALL,
+                isWeb: true,
+              ),
+            SizedBox(height: 15),
+            if (!controller.isPredicting && controller.predictedYield != -1)
+              Center(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          'YIELD PREDICTION',
+                          style: AppTheme.headingBoldText,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          'SEASON: ' + controller.selectedSeason,
+                          style: AppTheme.headingRegularText,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          'CROP: ' +
+                              controller.getCropNameFromCropId(
+                                controller.selectedCrop,
+                              ),
+                          style: AppTheme.headingRegularText,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          'PREDICTED YIELD: ' +
+                              controller.calculatePersonalisedYield() +
+                              ' quintals',
+                          style: AppTheme.headingRegularText,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     ),
