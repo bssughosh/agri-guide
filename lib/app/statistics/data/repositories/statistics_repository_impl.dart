271e95643520270a590b4cba7e60dc21d9ce28ec
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-03-28 12:23:00 +0530

    stats caching done

--- lib/app/statistics/data/repositories/statistics_repository_impl.dart
+++ lib/app/statistics/data/repositories/statistics_repository_impl.dart
@@ -18,8 +18,22 @@ class StatisticsRepositoryImpl implements StatisticsRepository {
     defaultValue: 'https://agri-guide-api.herokuapp.com',
   );
 
+  /// Key name `stateId`
+  Map<String, String> _stateNamesMap = {};
+
+  /// Key name `distId`
+  Map<String, String> _distNamesMap = {};
+
+  /// Key name `stateId/distId`
+  Map<String, StatisticsEntity> _statisticsData = {};
+
+  /// Key name `stateId/distId/cropId/season`
+  Map<String, YieldStatisticsEntity> _yieldStatisticsData = {};
+
   @override
   Future<StatisticsEntity> fetchWholeData(String state, String district) async {
+    if (_statisticsData.containsKey('$state/$district'))
+      return _statisticsData['$state/$district'];
     List<String> stateName = await _fetchStateNames(state);
     List<String> districtName = await _fetchDistNames(district);
 
@@ -52,53 +66,9 @@ class StatisticsRepositoryImpl implements StatisticsRepository {
       rainfallData: _rainfall,
     );
 
+    _statisticsData['$state/$district'] = statisticsEntity;
+
     return statisticsEntity;
-  }
-
-  _fetchStateNames(String stateId) async {
-    if (stateId == 'Test') {
-      return List<String>.from(['Test']);
-    }
-    String url = '$base_url/get_state_value?state_id=$stateId';
-    http.Response value = await http.get(Uri.parse(url));
-    if (value.statusCode == 400) {
-      throw APIBadRequestError();
-    } else if (value.statusCode == 403) {
-      throw APIForbiddenError();
-    } else if (value.statusCode == 404) {
-      throw APINotFoundError();
-    } else if (value.statusCode == 429) {
-      throw APITooManyRequestsError();
-    } else if (value.statusCode == 500) {
-      throw APIInternalServerError();
-    } else if (value.statusCode == 503) {
-      throw APIServiceUnavailabeError();
-    }
-    var data = json.decode(value.body);
-    return List<String>.from(data['states']);
-  }
-
-  _fetchDistNames(String districtId) async {
-    if (districtId == 'Test') {
-      return List<String>.from(['Test']);
-    }
-    String url = '$base_url/get_dist_value?dist_id=$districtId';
-    http.Response value = await http.get(Uri.parse(url));
-    if (value.statusCode == 400) {
-      throw APIBadRequestError();
-    } else if (value.statusCode == 403) {
-      throw APIForbiddenError();
-    } else if (value.statusCode == 404) {
-      throw APINotFoundError();
-    } else if (value.statusCode == 429) {
-      throw APITooManyRequestsError();
-    } else if (value.statusCode == 500) {
-      throw APIInternalServerError();
-    } else if (value.statusCode == 503) {
-      throw APIServiceUnavailabeError();
-    }
-    var data = json.decode(value.body);
-    return List<String>.from(data['dists']);
   }
 
   @override
@@ -108,6 +78,8 @@ class StatisticsRepositoryImpl implements StatisticsRepository {
     String cropId,
     String season,
   ) async {
+    if (_yieldStatisticsData.containsKey('$state/$district/$cropId/$season'))
+      return _yieldStatisticsData['$state/$district/$cropId/$season'];
     List<String> stateName = await _fetchStateNames(state);
     List<String> districtName = await _fetchDistNames(district);
 
@@ -137,6 +109,77 @@ class StatisticsRepositoryImpl implements StatisticsRepository {
     YieldStatisticsEntity yieldStatisticsEntity =
         new YieldStatisticsEntity(yieldData: _yield);
 
+    _yieldStatisticsData['$state/$district/$cropId/$season'] =
+        yieldStatisticsEntity;
+
     return yieldStatisticsEntity;
+  }
+
+  _fetchStateNames(String stateId) async {
+    if (stateId == 'Test') {
+      return List<String>.from(['Test']);
+    }
+    if (_stateNamesMap.containsKey(stateId)) {
+      return _stateNamesMap[stateId];
+    }
+    String url = '$base_url/get_state_value?state_id=$stateId';
+    http.Response value = await http.get(Uri.parse(url));
+    if (value.statusCode == 400) {
+      throw APIBadRequestError();
+    } else if (value.statusCode == 403) {
+      throw APIForbiddenError();
+    } else if (value.statusCode == 404) {
+      throw APINotFoundError();
+    } else if (value.statusCode == 429) {
+      throw APITooManyRequestsError();
+    } else if (value.statusCode == 500) {
+      throw APIInternalServerError();
+    } else if (value.statusCode == 503) {
+      throw APIServiceUnavailabeError();
+    }
+    var data = json.decode(value.body);
+    List<String> _output = List<String>.from(data['states']);
+
+    if (_output.length == 1) {
+      _stateNamesMap[stateId] = _output[0];
+    } else {
+      throw Exception('The output is not proper');
+    }
+
+    return _output;
+  }
+
+  _fetchDistNames(String districtId) async {
+    if (districtId == 'Test') {
+      return List<String>.from(['Test']);
+    }
+    if (_distNamesMap.containsKey(districtId)) {
+      return _distNamesMap[districtId];
+    }
+    String url = '$base_url/get_dist_value?dist_id=$districtId';
+    http.Response value = await http.get(Uri.parse(url));
+    if (value.statusCode == 400) {
+      throw APIBadRequestError();
+    } else if (value.statusCode == 403) {
+      throw APIForbiddenError();
+    } else if (value.statusCode == 404) {
+      throw APINotFoundError();
+    } else if (value.statusCode == 429) {
+      throw APITooManyRequestsError();
+    } else if (value.statusCode == 500) {
+      throw APIInternalServerError();
+    } else if (value.statusCode == 503) {
+      throw APIServiceUnavailabeError();
+    }
+    var data = json.decode(value.body);
+    List<String> _output = List<String>.from(data['dists']);
+
+    if (_output.length == 1) {
+      _distNamesMap[districtId] = _output[0];
+    } else {
+      throw Exception('The output is not proper');
+    }
+
+    return _output;
   }
 }
