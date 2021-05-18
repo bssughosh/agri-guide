f36af37d62f518d1f995a6f47519ef25f780f655
Sughosh Sudhanvan <bssughosh27@gmail.com>
2021-05-18 22:47:46 +0530

    removed invalid assertion

--- lib/core/observer.dart
+++ lib/core/observer.dart
@@ -8,13 +8,11 @@ class UseCaseObserver implements Observer<void> {
   UseCaseObserver(this._onComplete, this._onError, {this.onNextFunction});
   @override
   void onComplete() {
-    assert(_onComplete != null);
     _onComplete();
   }
 
   @override
   void onError(e) {
-    assert(_onError != null);
     _onError(e);
   }
 
